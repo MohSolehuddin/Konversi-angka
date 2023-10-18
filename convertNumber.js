a8ae8f7a1378abe44399dc2d86df55f4cded29c6
MohSolehuddin <moh.solehuddin190805@gmail.com>
2023-10-19 02:55:52 +0700

    perbaikan pemisah angka ribuan pada nilai desimal

--- convertNumber.js
+++ convertNumber.js
@@ -16,7 +16,7 @@ function convertNumber(valueFrom, from, to){
       if (notBiner.test(valueFrom)) {
         return false;
       } else {
-        return valueFrom.toLocaleString('in-id');
+        return valueFrom;
       }
     }
     //periksa apakah bilangan tersebut hexadesimal atau bukan
@@ -24,7 +24,7 @@ function convertNumber(valueFrom, from, to){
       if (notHexadecimal.test(valueFrom)) {
         return false;
       } else {
-        return valueFrom.toLocaleString('in-id');
+        return valueFrom;
       }
     }
     //periksa apakah bilangan tersebut octal atau bukan
@@ -32,12 +32,12 @@ function convertNumber(valueFrom, from, to){
       if (notOctal.test(valueFrom)) {
         return false;
       } else {
-        return valueFrom.toLocaleString('in-id');
+        return valueFrom;
       }
     }
     //jika desimal langsung return tanpa pemeriksaan
     if (from === "desimal") {
-      return valueFrom.toLocaleString('in-id');
+      return valueFrom;
     }
   }
   
@@ -48,13 +48,13 @@ function convertNumber(valueFrom, from, to){
    //desimal to
     if (from === "desimal") {
       if (to === "biner") {
-        return valueFrom.toString(2).toLocaleString('in-ID');
+        return valueFrom.toString(2);
       }
       if (to === "hexa") {
-        return valueFrom.toString(16).toLocaleString('in-ID');
+        return valueFrom.toString(16);
       }
       if(to === "octa") {
-        return valueFrom.toString(8).toLocaleString('in-ID');
+        return valueFrom.toString(8);
       }
     }
     // biner to
@@ -64,15 +64,15 @@ function convertNumber(valueFrom, from, to){
         return false;
       }else{
         if (to === "desimal") {
-          return parseInt(valueFrom,2).toLocaleString('in-ID');
+          return parseInt(valueFrom,2);
         }
         if (to === "hexa") {
           let desimal = parseInt(valueFrom, 2);
-          return desimal.toString(16).toLocaleString('in-ID');
+          return desimal.toString(16);
         }
         if (to === "octa") {
           let desimal = parseInt(valueFrom,2);
-          return desimal.toString(8).toLocaleString('in-ID');
+          return desimal.toString(8);
         }
       }
     }
@@ -87,16 +87,16 @@ function convertNumber(valueFrom, from, to){
         }
         if (to === "biner") {
           let desimal = parseInt(valueFrom,8);
-          return desimal.toString(2).toLocaleString('in-ID');
+          return desimal.toString(2);
         }
         if (to === "hexa") {
           let desimal = parseInt(valueFrom,8);
-          return desimal.toString(16).toLocaleString('in-ID');
+          return desimal.toString(16);
         }
       }
     }
   } else {
-   //hexa to
+    //hexa to
     //periksa apakah bilangan tersebut hexadesimal atau bukan
     if (notHexadecimal.test(valueFrom)) {
       return false;
@@ -106,17 +106,14 @@ function convertNumber(valueFrom, from, to){
       }
       if (to === "biner") {
         let desimal = parseInt(valueFrom,16);
-        // output += 
-        return desimal.toString(2).toLocaleString('in-ID');
+        return desimal.toString(2);
       }
       if (to === "octa") {
         let desimal = parseInt(valueFrom,16);
-        // output += 
-        return desimal.toString(8).toLocaleString('in-ID');
+        return desimal.toString(8);
       }
     } 
   }
-  return output
 }
 //membuat fungsi print untuk menampilkan ke html
 function printInput() {
@@ -126,25 +123,24 @@ function printInput() {
   //jika hexa maka input text, jika bukan maka number
   if (from === "hexa") {
     document.getElementById("input-group").innerHTML = `
-         <input type="text" name="valueFrom" id="valueFrom" placeholder="masukkan nilai ${from}" oninput="print()"/>
+         <input type="text" name="valueFrom" id="valueFrom" placeholder="masukkan nilai ${from}" oninput="printResultConvertion()"/>
   `
   }else{
   document.getElementById("input-group").innerHTML = `
-         <input type="number" name="valueFrom" id="valueFrom" placeholder="masukkan nilai ${from}" oninput="print()"/>
+         <input type="number" name="valueFrom" id="valueFrom" placeholder="masukkan nilai ${from}" oninput="printResultConvertion()"/>
   `}
 }
 
-function print() {
+function printResultConvertion() {
   // mengambil nilai input nilai yang ingin di convert
   let valueFrom = document.getElementById("valueFrom").value;
   // mengambil nilai input pilihan dari apa ke apa konversi akan dilakukan
   let from = document.getElementById("from").value;
   let to = document.getElementById("to").value;
-  //buat variabel value yang mendapat nilai dari function convertNumber
-  let value = convertNumber(valueFrom ,from ,to);
-  console.log(value);
+  //buat variabel resultConvertion yang mendapat nilai dari function convertNumber
+  let resultConvertion = convertNumber(valueFrom ,from ,to);
   // cegat jika input tidak sesuai dengan jenis bilangan
-  if (value === false ) {
+  if (resultConvertion === false ) {
     // cegat untuk masing masing bilangan
     if (from === "biner") {
       document.getElementById("output").innerHTML = `<h3 class="danger">Maaf data yang anda input bukanlah bilangan ${from}, bilangan ${from} hanya memiliki basis 2 yang terdiri dari: 0 dan 1 </h3>`;
@@ -155,11 +151,23 @@ function print() {
     }
   }
   // jika ternyata user tidak menginput maka beritau user untuk menginput
-  else if (value === null) {
+  else if (resultConvertion === null) {
     document.getElementById("output").innerHTML = `<h3>silahkan masukkan nilai ${from}</h3>`; 
   }
   // jika user tidak menginput nilai yang salah maka keluarkan nilai
   else{
-   document.getElementById("output").innerHTML = `<h3>Hasil konversi dari ${from.toLocaleString("in-id")} dengan nilai ${valueFrom.toLocaleString()} ke ${to.toLocaleString('in-id')} adalah: </h3><span class="output">${value}</span>`; 
+    // memberikan  pemisah ribuan yang baik dan benar pada angka desimal
+    if (to === "desimal" || from === "desimal") {
+      if (from === "desimal" && to !== "desimal") {
+        document.getElementById("output").innerHTML = `<h3>Hasil konversi dari ${from} dengan nilai ${Number(valueFrom).toLocaleString('in-id')} ke ${to} adalah: </h3><span class="output">${resultConvertion}</span>`;
+      }
+      else if (to === "desimal" && from !== "desimal"){
+        document.getElementById("output").innerHTML = `<h3>Hasil konversi dari ${from} dengan nilai ${valueFrom} ke ${to} adalah: </h3><span class="output">${Number(resultConvertion).toLocaleString("in-id")}</span>`
+      }else{
+      document.getElementById("output").innerHTML = `<h3>Hasil konversi dari ${from} dengan nilai ${Number(valueFrom).toLocaleString('in-id')} ke ${to} adalah: </h3><span class="output">${Number(resultConvertion).toLocaleString("in-id")}</span>`;
+      }
+    } else {
+      document.getElementById("output").innerHTML = `<h3>Hasil konversi dari ${from} dengan nilai ${valueFrom} ke ${to} adalah: </h3><span class="output">${resultConvertion}</span>`
+    }
   }
 }
